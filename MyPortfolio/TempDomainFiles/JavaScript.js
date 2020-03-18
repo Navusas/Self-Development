98b49525a5b4e3f3aee99f294eb1144ec5553285
Domantas Giedraitis <U1757704@unimail.hud.ac.uk>
2020-03-18 21:36:14 +0000

    Set-up initial index page to be displayed in the domantasg.com hosting

---
+++ MyPortfolio/TempDomainFiles/JavaScript.js
@@ -0,0 +1,44 @@
+﻿var i = 0;
+var txt = 'This website is currently under development...';
+var speed = 40;
+var firstTime = true;
+
+
+var j = 0;
+var secondSpeed = 40;
+var secondTXT = "In the meantime, visit my LinkedIn:"
+var secondTime = true;
+
+function firstLineWritter() {
+    if (i < txt.length) {
+        document.getElementById("firstLine").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(firstLineWritter, speed);
+    }
+    else {
+        if (firstTime) {
+            setTimeout(firstLineWritter, 200);
+            firstTime = false;
+        }
+        else {
+            secondLineWritter();
+        }
+    }
+}
+
+function secondLineWritter() {
+    if (j < secondTXT.length) {
+        document.getElementById("secondLine").innerHTML += secondTXT.charAt(j);
+        j++;
+        setTimeout(secondLineWritter, secondSpeed);
+    }
+    else {
+        if (secondTime) {
+            setTimeout(secondLineWritter, 200);
+            secondTime = false;
+        }
+        else {
+            document.getElementById("urlID").style.display = "block";
+        }
+    }
+}
